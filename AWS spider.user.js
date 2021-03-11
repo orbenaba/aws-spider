f4b87bfa14952dad38a9c0b78d5520374248178b
benaba <55410409+orbenaba@users.noreply.github.com>
2021-03-11 04:35:09 +0200

    Update AWS spider.user.js

--- AWS spider.user.js
+++ AWS spider.user.js
@@ -11,16 +11,62 @@
 // @require http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
+const create_logs_table = ()=>{
+    var logs_table = $("<div id='logs_table'></div>");   // Create with jQuery
+
+    $("body").append(logs_table);      // Append the new elements
+
+
+//console.log("------------------------------------ = ", localStorage.getItem("logs"));
+
+
+    var $table = $('<table/>');
+
+
+    var logsArray = localStorage.getItem("logs").split("#@!@");
+    logsArray.forEach(ele => console.log(ele));
+
+    for(var i = 0; i < logsArray.length; i += 2){
+        $table.append( '<tr><td>' + logsArray[i] + '</td><td>' + logsArray[i+1] +'</td></tr>' );
+    }
+    $('#logs_table').append($table);
+    $("body").append(logs_table);      // Append the new elements
+}
+
+
 (function() {
     'use strict';
 
     // Targeting the name element and logging it into the console
-    var activeAccount = (document.getElementsByClassName("ThRjn7o-KwO0459UzmvoU w8Kxy2XztOAkWobGpdJLt")[0]).textContent;
-    console.log("activeAccount = ", activeAccount);
+    let activeAccount = (document.getElementsByClassName("ThRjn7o-KwO0459UzmvoU w8Kxy2XztOAkWobGpdJLt")[0]).textContent;
 
-    // Save the active account into a file localy,
-    var a = document.createElement("a");
-    a.href = `data:text,${activeAccount}`; //content
-    a.download = "log.txt"; //file name
-    a.click();
+    // #@!@ is the delimiter which splits the string
+    let log = `${activeAccount}#@!@${new Date()}#@!@\n`;
+    // Saving the data as JSON in localStorage
+//        localStorage.setItem("logs", []);
+    localStorage.setItem("logs", log + localStorage.getItem("logs"));
+
+
+    // Log the results into the console
+    // console.log("local storage dot storage = ", JSON.stringify(localStorage.getItem("logs")).length);
+
+
+    // Popup confirmation
+    if(confirm("Wanna see the logs for your AWS account?")){
+        // Clear the site UI and replace it with new table which shows the logs
+        window.onload=function(){
+            document.body.innerHTML = "";
+            create_logs_table();
+        }
+    }
+    else{
+        // Nothing
+    }
+
+// Just in case, Tzur would like to save the logs on the Disk and not on local storage
+// Save the active account into a file localy,
+//    let a = document.createElement("a");
+//    a.href = `data:text,${localStorage.getItem("logs")}`; //content
+//    a.download = "log.txt"; //file name
+//    a.click();
 })();
